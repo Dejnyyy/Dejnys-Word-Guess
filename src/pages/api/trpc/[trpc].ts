d93c7c349349f19434777de896700e8550c95ce2
Dejnyyy <danslaby@email.cz>
2024-12-18 09:02:12 -0500

    zbytecne to tam bylo

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,7 +1,6 @@
-export const runtime = 'edge';
-
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
+import { env } from "~/env";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
@@ -10,7 +9,7 @@ export default createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
   onError:
-    process.env.NODE_ENV === "development"
+    env.NODE_ENV === "development"
       ? ({ path, error }) => {
           console.error(
             `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
