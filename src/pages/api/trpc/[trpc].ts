d66ee7ab3bd0d683bb102a2e67df964ea466a5a2
Dejnyyy <danslaby@email.cz>
2024-12-18 08:51:17 -0500

    dasdas

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,15 +1,17 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
-import { env } from "~/env";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
-// export API handler
+// Edge Runtime requires explicit runtime export
+export const runtime = "edge";
+
+// Adapted tRPC handler for Cloudflare Edge Runtime
 export default createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
   onError:
-    env.NODE_ENV === "development"
+    process.env.NODE_ENV === "development"
       ? ({ path, error }) => {
           console.error(
             `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
